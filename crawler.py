476337611797814913a597cf9b40e271785b921a
emmetthitz <emmetthitz@gmail.com>
2014-09-01 14:59:16 -0400

    Basic command line interfacing

--- crawler.py
+++ crawler.py
@@ -1,1 +1,2 @@
-import facebook
+# import facebook
+import urllib
--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-from crawler.py import *
-from database.py import *
-
+from crawler import *
+from database import *
+import sys
 
 
 if __name__=="__main__":
-	#-pr to crawl public records via -crawler, -fb to crawl facebook
-	#-a for raw stats from -db, -p to make predictions
-	print "Vest v0.1: \nOptions: -crawler, -data\nFlags implemented for -crawler:\nFlags implemented for -data:\n"
+	# runs on python 3.4 or later
+	# -crawler flags: -pr to crawl public records, -fb to crawl facebook
+	# -db flags: -d for full table dump, -a for raw stats, -p to make predictions
+	print ("Vest v0.1: \nOptions: -crawler, -data\nFlags implemented for -crawler:\nFlags implemented for -data:\n")
+	inputText=input("Enter a command:\n").strip().toLower().replace("  ","")
+	print (inputText)
+	inputArgs=inputText.split(" ")
+	if (inputArgs[0]=="-crawler"):
+		crawler=Crawler()
+		if (inputArgs[1]=="-pr"):
+			crawler.crawlPR()
+		elif (inputArgs[1]=="-fb"):
+			crawler.crawlFB()
+		else 
+			print ("Invalid crawler flags")
+			sys.exit()
+	elif (inputArgs[0]=="-db"):
+		db=Database()
+		if (inputArgs[1]=="-d"):
+			db.dump()
+		elif (inputArgs[1]=="-a")
+			db.stats()
+		elif (inputArgs[1]=="-p")
+			db.predict()
+		else
+			print ("Invalid db flags")
+			sys.exit()
+	else
+		print ("Invalid options")
+		sys.exit()
+
+
+
+
+	
