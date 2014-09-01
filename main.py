6903d1f01981b2471ba007c649de6bede1cc21be
emmetthitz <emmetthitz@gmail.com>
2014-09-01 13:09:39 -0400

    Fleshing out the basics

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-import facebook
+from crawler.py import *
+from database.py import *
+
+
+
+if __name__=="__main__":
+	#-pr to crawl public records via -crawler, -fb to crawl facebook
+	#-a for raw stats from -db, -p to make predictions
+	print "Vest v0.1: \nOptions: -crawler, -data\nFlags implemented for -crawler:\nFlags implemented for -data:\n"
