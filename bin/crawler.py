a2642be06937cd1710325b802171b0bfde26cacc
paulrevere4 <revereiv@gmail.com>
2014-09-10 15:30:29 -0400

    Facebook login working, time to start pulling links.

--- bin/crawler.py
+++ bin/crawler.py
@@ -1,93 +1,126 @@
-#from facebook_sdk import *
-import os.path
-import json
-import urllib2
+import sys
+import getpass
 import urllib
-import cookielib
-import gzip
-import StringIO
+import random
+import requests
 import urlparse
-import BaseHTTPServer
-import webbrowser
-import re
+from bs4 import BeautifulSoup
 import parse_url
 
-class Crawler:
-	# will have this info passed in the future
-	self.email = "***@gmail.com"
-	self.passwd = "***"
-	self.cookies = cookielib.CookieJar()
-	self.headers=
-	{
-		'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:13.0) Gecko/20100101 Firefox/13.0.1',
-		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-		'Accept-Language':'en-us,en;q=0.5',
-		#'Accept-Encoding':'gzip',
-		'Connection':'keep-alive',
-		'Cache-Control':'max-age=3600',
-		'Content-Type':'application/x-www-form-urlencoded'
-	}
-	self.urls=	
-	{
-		"findfriends":"http://www.facebook.com/find-friends/browser/",
-		"facebook":"http://www.facebook.com/"
-	}
+###
+# Global Config
+###
 
-	def getPage(url,data=''):
-		"""Returns page's HTML.
-		Args[0] = URL
-		Args[1] = Data
-		"""
-		req = urllib2.Request(url,headers=headers)
-		response = urllib2.urlopen(req)
-		return response
+username = raw_input("[+] Facebook Email: ")
+password = getpass.getpass("[+] Facebook Password (will not show): ")
+session     = None
+facebook_id = ""
 
-	def login(email,passwd):
-		"""Logs into facebook with passed credentials
-		"""
-		url="http://www.facebook.com/index.php"
-		response=getPage(url) # Go to facebook.com to get initial cookies
-		# Should be able to get all this info (exept u/n and p/w) from response. But how?
-		opts= 
-		(
-		('lsd','AVrB8vRK'),
-		('email',email),
-		('pass',passwd),
-		('persistent','1'), # 0 or 1 for persistant to not
-		('default_persistent','1'),
-		('charset_test','%E2%82%AC%2C%C2%B4%2C%E2%82%AC%2C%C2%B4%2C%E6%B0%B4%2C%D0%94%2C%D0%84'),
-		('timezone','240'),
-		('return_session','0'),
-		('legacy_return','1'),
-		('display',''),
-		('session_key_only','0'),
-		('lgnrnd','191955_7tXF'),
-		('lgnjs','n'),
-		('login','Log+In')	
-		)
+#!# End Config #!#
 
-		data = urllib.urlencode(opts)
-		print data
-		request = urllib2.Request(url, data, headers, origin_req_host="https://www.facebook.com/login.php?login_attempt=1") # Should req_host be part of the header?
-		print request
-		self.cookies.extract_cookies(response,request)
-		cookie_handler= urllib2.HTTPCookieProcessor( self.cookies )
-		redirect_handler= urllib2.HTTPRedirectHandler()
-		opener = urllib2.build_opener(redirect_handler,cookie_handler)
-		response = opener.open(request)
+# Misc Functions
+    
+def show_status(message):
+    sys.stdout.write("[+] %s, " % message)
+    sys.stdout.flush()
+
+def get_random_useragent():
+    base_agent = "Mozilla/%.1f (Windows; U; Windows NT 5.1; en-US; rv:%.1f.%.1f) Gecko/%d0%d Firefox/%.1f.%.1f"
+    return base_agent % ((random.random() + 5),
+                         (random.random() + random.randint(1, 8)), random.random(),
+                         random.randint(2000, 2100), random.randint(92215, 99999),
+                         (random.random() + random.randint(3, 9)), random.random())
+
+def parse_forms(page_url, page_content):
+    soup = BeautifulSoup(page_content)
+
+    forms = []
+    for form in soup.findAll("form"):
+        form_action = form["action"]
+        if not form_action.startswith("http"):
+            form_action = urlparse.urljoin(page_url, form_action)
+        form_data = {}
+
+        # Get input tags
+        for input in form.findAll("input"):
+            if not input.has_attr("name") or input.has_attr("onclick"):
+                continue
+            if input.has_attr("type") and input["type"] == "checkbox":
+                continue
+            input_name = input["name"]
+            if input.has_attr("value"):
+                input_value = input["value"]
+            else:
+                input_value = ""
+            form_data[input_name] = input_value
+
+        # Get select statements
+        for select in form.findAll("select"):
+            options = []
+            select_name = select["name"]
+            for option in select.findAll("option"):
+                options.append(option["value"])
+            form_data[select_name] = options
+
+        # Get textareas
+        for textarea in form.findAll("textarea"):
+            textarea_name = textarea["name"]
+            form_data[textarea_name] = ""
+
+        form = {"action": form_action, "inputs": form_data}
+        forms.append(form)
+
+    return forms
+
+
+# Main Functions
+
+def login():
+    global facebook_id
+    show_status("Logging in")
+
+    start_url = "http://facebook.com/"
+    page = session.get(start_url).content.decode('utf-8', 'replace')
+    forms = parse_forms(start_url, page)
+    form = forms[0]
+    form_action = form["action"]
+    referer = form_action
+    form_data = form["inputs"]
+    form_data['email'] = username
+    form_data['pass'] = password
+    request = session.post(form_action, form_data)
+
+    print "Done"
+
+    if "Log Out" not in request.content:
+        return False
+
+    #facebook_id = request.content.split("({\"user\":\"")[1].split("\"")[0]
+
+    return True
+
+
+
+if __name__ == '__main__':
+    session = requests.session()#headers={"User-Agent": get_random_useragent()})
+
+    # Log on in
+    if not login():
+        print "+" * 85
+        print "[!] Error!"
+        print "[!] Failed to log in to Facebook with the username and password provided."
+        raw_input("[+] Press enter to exit...")
+        exit()
+
+    print
+    print "[!] Login successful."
+    
+    print
+    print "-" * 85
+    print "[+] All done..."
+    raw_input("[+] Press enter to exit...")
+
+
+
+
 		
-		# Decode gzip encoding
-		if response.info().get('Content-Encoding') == 'gzip':
-			buf = StringIO.StringIO( response.read())
-			f = gzip.GzipFile(fileobj=buf)
-			data = f.read()
-			return data
-		else:
-			return response.read()
-
-	if __name__=="__main__":
-		
-		# Write output to fb.html
-		f = open('./fb.html', 'w') # Check fb.html to see if we are logged in.
-		f.write(login(email,passwd))
-		f.close()
--- bin/parse_url.py
+++ bin/parse_url.py
@@ -1,6 +1,49 @@
 import urllib 
 import sys
 
+def next_url( fstring, start_loc ):
+    """ Get the next url from the fstring starting at the start_loc
+        index.  Return a tuple giving the index of the first character
+        in the URL in fstring, followed by one-past the last character.
+    """
+    end_pair = (-1,0)   # What to return if no valid URL is found.
+
+    # Find the <a
+    a_loc = fstring.find('<a', start_loc)
+    if a_loc == -1:  return end_pair
+
+    # Find the href
+    href_loc = fstring.find( 'href', start_loc+1 )
+    if href_loc == -1:  return end_pair
+
+    # We have an href, so let's find the "
+    url_loc = fstring.find( '"', href_loc+1 )
+    if url_loc == -1: return end_pair
+
+    # Find the matching end "
+    end_url_loc = fstring.find( '"', url_loc+1)
+    if end_url_loc == -1: return end_pair
+
+    # Now we've got it!
+    return (url_loc+1, end_url_loc )
+
+# ------------------------------------------------------------------------
+
+def skip_link(link):
+    """ A quick test to determine which links to eliminate based on
+        being a relative link or using the wrong protocol.
+    """
+    return \
+        '#' in link or '..' in link or '.pdf' in link or '.ps' in link or \
+        '.png' in link or '.gif' in link or 'java' in link or \
+        link.startswith("ftp:") or link.startswith("file:") or \
+        link.startswith("gopher:") or link.startswith("https:") or \
+        link.startswith("ws:") or link.startswith("wss:") or \
+        'mailto' in link
+
+# ------------------------------------------------------------------------
+
+
 def get_links(url):
     """" Open the URL and extract all the links, storing them in the
          order visited in a *list*.
