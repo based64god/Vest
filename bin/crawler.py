3457ffe01df1909a5bf4d1e0def41e6cfea5df88
paulrevere4 <revereiv@gmail.com>
2014-10-24 21:58:20 -0400

    Scrolling for scraping implemented, having issues with getting the page source. Must install Firefox and Selenium for it to work. Also committing this from Brooklyn!

--- bin/crawler.py
+++ bin/crawler.py
@@ -1,66 +1,20 @@
-import sys
 import getpass
-import urllib
-import random
-import requests
-import urlparse
-from bs4 import BeautifulSoup
-import parse_url
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 ###
 # Global Config
 ###
 
-username = input("[+] Facebook Email: ")
-password = getpass.getpass("[+] Facebook Password (will not show): ")
-session     = None
-facebook_id = ""
+username = raw_input("[+] Facebook Email: ")
+password = getpass.getpass("[+] Facebook Password (will not show): ") 
+driver = webdriver.Firefox()
+
 
 #!# End Config #!#
 
 # Misc Functions
-
-def parse_forms(page_url, page_content):
-    soup = BeautifulSoup(page_content)
-
-    forms = []
-    for form in soup.findAll("form"):
-        form_action = form["action"]
-        if not form_action.startswith("http"):
-            form_action = urlparse.urljoin(page_url, form_action)
-        form_data = {}
-
-        # Get input tags
-        for input in form.findAll("input"):
-            if not input.has_attr("name") or input.has_attr("onclick"):
-                continue
-            if input.has_attr("type") and input["type"] == "checkbox":
-                continue
-            input_name = input["name"]
-            if input.has_attr("value"):
-                input_value = input["value"]
-            else:
-                input_value = ""
-            form_data[input_name] = input_value
-
-        # Get select statements
-        for select in form.findAll("select"):
-            options = []
-            select_name = select["name"]
-            for option in select.findAll("option"):
-                options.append(option["value"])
-            form_data[select_name] = options
-
-        # Get textareas
-        for textarea in form.findAll("textarea"):
-            textarea_name = textarea["name"]
-            form_data[textarea_name] = ""
-
-        form = {"action": form_action, "inputs": form_data}
-        forms.append(form)
-
-    return forms
-
 
 # Main Functions
 
@@ -68,31 +22,40 @@ def login():
     global facebook_id
     print ("[!] Logging in.")
 
-    start_url = "http://facebook.com/"
-    page = session.get(start_url).content.decode('utf-8', 'replace')
-    forms = parse_forms(start_url, page)
-    form = forms[0]
-    form_action = form["action"]
-    referer = form_action
-    form_data = form["inputs"]
-    form_data['email'] = username
-    form_data['pass'] = password
-    request = session.post(form_action, form_data)
+    driver.get("https://www.facebook.com/")
+
+    assert "Facebook" in driver.title
+    elem = driver.find_element_by_id("email")
+    elem.send_keys(username)
+    elem = driver.find_element_by_id("pass")
+    elem.send_keys(password)
+    elem.send_keys(Keys.RETURN)
+
+    time.sleep(5)
 
     print ("Done")
-
-    if "Log Out" not in request.content:
-        return False
 
     #facebook_id = request.content.split("({\"user\":\"")[1].split("\"")[0]
 
     return True
 
-def get_friends():
-    page = requests.session().get("https://www.facebook.com/paul.revereiv/friends")
-    print (page.text)
+def scroll_friends():
+    print ("[!] loading friends...")
+    driver.get("https://www.facebook.com/paul.revereiv/friends")
+
+    #print driver.page_source
+
+    scroll_downs = 50
+    while scroll_downs > 0:
+        time.sleep(1)
+        driver.execute_script("window.scrollBy(0, 3000);")
+        scroll_downs -= 1
+
+    print ("Done")
 
 def parse_fb_friend_page(text): #function that when given the html of a friends page will return the id's of the person's friends
+
+    print ("[!] parsing...")
     friends = []
     for i in range(len(text)):
         if(text[i:i+8] == '<a href='):
@@ -101,11 +64,14 @@ def parse_fb_friend_page(text): #function that when given the html of a friends 
                 j = j + 1
             if( not '/' in text[i+34:j] and not '.php' in text[i+34:j]):
                 friends.append(text[i+34:j])
+    
+    print ("[!] Done")
     return friends
 
 
+
+
 if __name__ == '__main__':
-    session = requests.session()
 
     # Log on in
     if not login():
@@ -115,7 +81,8 @@ if __name__ == '__main__':
         input("[+] Press enter to exit...")
         exit()
     else:
-        get_friends()
+        scroll_friends()
+        parse_fb_friend_page(driver.page_source)
 
     print ()
     print ("[!] Login successful.")
