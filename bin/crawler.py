0df6b1f0bc3463c393d991baefa7b413bfd6e6d0
paulrevere4 <revereiv@gmail.com>
2015-02-21 18:38:21 -0500

    I forgot how to spell on my last commit, cleaned up code and commented

--- bin/crawler.py
+++ bin/crawler.py
@@ -40,16 +40,21 @@ class FBCrawler(object):
             #the function to return false
 
 
-        #scrolls down the friend page of a given id that the user account is friends with
-        #returns text of the html of the page
+        #This function used to scroll down the page to get all of a person's friends
+        #It has since been changed to use the mobile fb site which loads friend lists as a series of pages
+        #This function now runs though all of the pages and parses them to find all of the friends they contain
+        #Input: an fb_id
+        #Returns: a list of the id's friends
     def get_friends(this, fb_id):
         print ("[!] loading friends...")
         this.driver.get("m.facebook.com/%s?v=friends" %fb_id) #load the page
         friends = []
+        #get the source of the page so we can parse it
         page_src = ""
         elements = this.driver.find_elements_by_tag_name('body') #get all of the html in a list of WebElement objects
         for elem in elements:
             page_src += elem.get_attribute('innerHTML')  #add the text of each element to a big string for parsing
+        #we need to determine how many friends the person has so we find it when we parse the first time
         num_friends_str = ""
         for i in range(len(page_src)):
             if page_src[i:i+9] == "Friends (":
@@ -58,57 +63,45 @@ class FBCrawler(object):
                     j = j + 1
                 num_friends_str = page_src[i+9:j]
                 break
-        num_friends_int = int(num_friends_str.replace(",",""))
-        print num_friends_int
+        num_friends_int = int(num_friends_str.replace(",","")) #turn the string that contains the number of friends into an int
+        #print num_friends_int
         page_src = ""
-        n = 0
+        n = 0 #counter to hold the start of the range of friends we are looking at
         while n < num_friends_int:
-            elements = this.driver.find_elements_by_tag_name('body') #get all of the html in a list of WebElement objects
-            for elem in elements:
-                page_src += elem.get_attribute('innerHTML')  #add the text of each element to a big string for parsing
-            friends.extend(this.parse_fb_friend_page(page_src))
-            page_src = ""
-            if n == 0:
+            elements = this.driver.find_elements_by_tag_name('body') #get the source of the page again
+            for elem in elements:                           
+                page_src += elem.get_attribute('innerHTML')
+            friends.extend(this.parse_fb_friend_page(page_src)) #parse each page to get the friends on it
+            page_src = "" 
+            if n == 0: #for some first page only shows 24 friends and the rest show 36, this accounts for that
                 n = 24
             else:
                 n = n + 36
-            this.driver.get("https://m.facebook.com/%s?v=friends&mutual&startindex=%d" %(fb_id,n))
-            #time.sleep(.1)
+            this.driver.get("https://m.facebook.com/%s?v=friends&mutual&startindex=%d" %(fb_id,n)) 
+            #load the next page containing the person's friends, 
+                #startindex=%d picks the range of friends (n to n+36) that will be shown
             
         print ("[!] Done loading")
-        print "len(friends) = %d" %len(friends)
+        #print "len(friends) = %d" %len(friends)
         return friends
 
 
         #takes html text of friends page
         #returns list of strings of the id's of the person's friends
     def parse_fb_friend_page(this, text):
-        print ("[!] parsing...")
+        #print ("[!] parsing...")
         friends = []
         for i in range(len(text)):
             if(text[i:i+8] == '<a href='): #look for links and add the id the link points to
                 j = i
                 while(j < len(text) and text[j] != '>' ):
                     j = j + 1
-                if 'fr_tab' in text[i:j]:
+                if 'fr_tab' in text[i:j] and not 'profile.php' in text[i:j]:
                     k = i
                     while(k < len(text) and text[k] != '?'):
                         k = k + 1
                     friends.append(text[i+10:k])
 
-            '''
-                j = i
-                while(j < len(text) and text[j] != '>' ):
-                    j = j + 1
-                if 'friends_tab' in text[i:j]:
-                    k = i
-                    while(k < len(text) and text[k] != '?'):
-                        k = k + 1
-                    if text[i+34:k] != 'profile.php':
-                        friends.append(text[i+34:k])
-            '''
-        
-        print ("[!] Done parsing")
         return friends
 
 
