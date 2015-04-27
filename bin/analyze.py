9350aaafd27060992f035c9f56ba457c9b47e497
paulrevere4 <revereiv@gmail.com>
2015-04-26 22:02:52 -0400

    Faster unweighted analysis

--- bin/analyze.py
+++ bin/analyze.py
@@ -15,24 +15,34 @@ def unweighted_ranking(victims_dict):
 
 	'''
 
-	ranks = {}
-	for v in victims_dict.keys():
+	all_ids = []
+	for v in victims_dict.keys(): #add all of the ids to a big list
 		for _id in victims_dict[v]:
-			if _id in ranks.keys():
-				ranks[_id] += 1
-			else:
-				ranks[_id] = 1
-	ranked_list = [] #list of lists that contain the _ids that occur at a given frequency
+			all_ids.append(_id)
+	all_ids = sorted(all_ids) #sort that list to count the occurances
+
+	ranked_list = [] #list of lists that contain the ids that occur at a given frequency
 	#i.e. ranked_list[1] = the ids that occur once
 	#	  ranked_list[4] = the ids that occur 4 times
-	while len(ranked_list)-1 < len(victims_dict.keys()):
+
+	#append empty lists to the ranking so that it can hold all of the occurances
+	#	it must be the length of the number of victims because that is the most times an _id can appear
+	while len(ranked_list)-1 < len(victims_dict.keys()): 
 		ranked_list.append( [] )
-	for _id in ranks.keys():
-		ranked_list[ ranks[_id] ].append(_id)
+
+	current_id = ""
+	current_id_count = 0
+	for _id in all_ids:
+		if current_id != _id:
+			ranked_list[ current_id_count ].append(current_id)
+			current_id = _id
+			current_id_count = 1
+		else:
+			current_id_count += 1
 
 	return ranked_list
 					
 
 
 if __name__ == "__main__":
-	print ("[!] This class has no main, use main.py")
+	print ("[!] This file has no main, use main.py")
